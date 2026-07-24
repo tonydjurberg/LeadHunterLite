8d86d249450850a467f934d9ad1ee9fb60d138a4
tonydjurberg <mrtonydjurberg@gmail.com>
2026-07-24 09:26:02 +0100

    Resolve merge conflict in scraper.js

--- scraper.js
+++ scraper.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const puppeteer = require('puppeteer');
 
 async function scrape() {
@@ -16,23 +15,4 @@ async function scrape() {
   await browser.close();
 }
 
-=======
-const puppeteer = require('puppeteer');
-
-async function scrape() {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-
-  const url = "https://www.industritorget.se/";
-
-  await page.goto(url, { waitUntil: "domcontentloaded" });
-
-  console.log("Vi är inne på sidan:", url);
-
-  await page.waitForTimeout(5000);
-
-  await browser.close();
-}
-
->>>>>>> 7f123d51918647eba740bee272440863e47d10d7
 scrape();
